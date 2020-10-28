a0fba6ad6c2209e907ad297d3ecb02bf9bd2ab60
wei_chen <wei_chen@mail.chungyo.net>
2020-10-28 11:34:50 +0800

    readme.md

--- database/migrations/2020_09_23_092938_import_dump.php
+++ database/migrations/2020_09_23_092938_import_dump.php
@@ -14,28 +14,29 @@ class ImportDump extends Migration
      */
     public function up()
     {
-        DB::unprepared('
-            CREATE TABLE `userDetail`(id int(10) ZEROFILL NOT NULL ,account varchar(30),orderID int(10) ZEROFILL NOT NULL,datatime timestamp  DEFAULT CURRENT_TIMESTAMP,actionName varchar(30),price int(30) UNSIGNED,status varchar(30),sellerID int(10) ZEROFILL NOT NULL);
-            CREATE TABLE `product` (id int(10) ZEROFILL NOT NULL,productID int(10) ZEROFILL NOT NULL PRIMARY KEY  AUTO_INCREMENT,datatime timestamp  DEFAULT CURRENT_TIMESTAMP,name varchar(30)NOT NULL,category varchar(30)NOT NULL,quantity int(10) UNSIGNED NOT NULL,quantitySold int(10) UNSIGNED NOT NULL,price int(10) UNSIGNED NOT NULL,description varchar(200),img longblob NOT NULL);
-            CREATE TABLE `inventory` (id int(10) ZEROFILL NOT NULL ,productID int(10) ZEROFILL NOT NULL ,quantity int(10) UNSIGNED NOT NULL,quantitySold int(10));
-            CREATE TABLE `order` (orderID int(10) ZEROFILL NOT NULL ,datatime timestamp  DEFAULT CURRENT_TIMESTAMP, id int(10) ZEROFILL NOT NULL ,sellerID int(10) ZEROFILL NOT NULL,productID int(10) ZEROFILL NOT NULL ,quantity int(10) UNSIGNED NOT NULL,price int(10) UNSIGNED NOT NULL);
-            CREATE TABLE `shopCart` (id int(10) ZEROFILL NOT NULL ,sellerID int(10) ZEROFILL NOT NULL,productID int(10) ZEROFILL NOT NULL ,quantity int(10) UNSIGNED NOT NULL,price int(10) UNSIGNED NOT NULL);
+        DB::unprepared(file_get_contents('/shopping/database/homestead.sql'))
+        // DB::unprepared('
+        //     CREATE TABLE `userDetail`(id int(10) ZEROFILL NOT NULL ,account varchar(30),orderID int(10) ZEROFILL NOT NULL,datatime timestamp  DEFAULT CURRENT_TIMESTAMP,actionName varchar(30),price int(30) UNSIGNED,status varchar(30),sellerID int(10) ZEROFILL NOT NULL);
+        //     CREATE TABLE `product` (id int(10) ZEROFILL NOT NULL,productID int(10) ZEROFILL NOT NULL PRIMARY KEY  AUTO_INCREMENT,datatime timestamp  DEFAULT CURRENT_TIMESTAMP,name varchar(30)NOT NULL,category varchar(30)NOT NULL,quantity int(10) UNSIGNED NOT NULL,quantitySold int(10) UNSIGNED NOT NULL,price int(10) UNSIGNED NOT NULL,description varchar(200),img longblob NOT NULL);
+        //     CREATE TABLE `inventory` (id int(10) ZEROFILL NOT NULL ,productID int(10) ZEROFILL NOT NULL ,quantity int(10) UNSIGNED NOT NULL,quantitySold int(10));
+        //     CREATE TABLE `order` (orderID int(10) ZEROFILL NOT NULL ,datatime timestamp  DEFAULT CURRENT_TIMESTAMP, id int(10) ZEROFILL NOT NULL ,sellerID int(10) ZEROFILL NOT NULL,productID int(10) ZEROFILL NOT NULL ,quantity int(10) UNSIGNED NOT NULL,price int(10) UNSIGNED NOT NULL);
+        //     CREATE TABLE `shopCart` (id int(10) ZEROFILL NOT NULL ,sellerID int(10) ZEROFILL NOT NULL,productID int(10) ZEROFILL NOT NULL ,quantity int(10) UNSIGNED NOT NULL,price int(10) UNSIGNED NOT NULL);
             
-            DELIMITER $$
-            CREATE TRIGGER ins_product
-            AFTER INSERT ON product FOR EACH ROW BEGIN
-            INSERT INTO inventory (id, productID, quantity)
-            Values (new.id, new.productID, new.quantity);
-            END;
-            $$
-            DELIMITER $$
-            CREATE TRIGGER upd_product
-            AFTER INSERT ON product FOR EACH ROW BEGIN
-            UPDATE inventory SET quantity = new.quantity
-            WHERE id = new.id && productID = new.productID;
-            END;
-            $$
-        ');
+        //     DELIMITER $$
+        //     CREATE TRIGGER ins_product
+        //     AFTER INSERT ON product FOR EACH ROW BEGIN
+        //     INSERT INTO inventory (id, productID, quantity)
+        //     Values (new.id, new.productID, new.quantity);
+        //     END;
+        //     $$
+        //     DELIMITER $$
+        //     CREATE TRIGGER upd_product
+        //     AFTER INSERT ON product FOR EACH ROW BEGIN
+        //     UPDATE inventory SET quantity = new.quantity
+        //     WHERE id = new.id && productID = new.productID;
+        //     END;
+        //     $$
+        // ');
     }
 
     /**
--- readme.md
+++ readme.md
@@ -1,10 +1,10 @@
 # 購物網專案
 
-## 
-## 執行composer install
+## 安裝套件
+執行composer install
 ## 設定ubuntu hosts
 vi /etc/hosts
 ### 加入以下兩行
-127.0.0.1	www.admin.net
-127.0.0.1	www.shopping.net
+127.0.0.1	www.admin.net <br>
+127.0.0.1	www.shopping.net <br>
 ## 
